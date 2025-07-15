6471d8ff4ef1ccf256d5373c0e288fb3e1753bbf
rksab <ramansarabha@gmail.com>
2025-07-15 16:22:19 +0530

    first commit

---
+++ frontend/src/services/person.jsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const url = 'http://localhost:3001/api/persons'
+
+const getPersons = () => {
+    const req = axios.get(url)
+    return req.then(res => res.data)
+}
+
+const addPersons = (data) => {
+    const req =  axios.post(url, data)
+    return req.then(res => res.data)
+}
+
+const updatePerson = (id, person) => {
+    const req = axios.put(`${url}/${id}`, person)
+    return req.then(res => res.data)
+}
+const deletePerson = (id)  => {
+    return axios.delete(`${url}/${id}`)
+}
+
+
+export { getPersons, addPersons, updatePerson, deletePerson}
