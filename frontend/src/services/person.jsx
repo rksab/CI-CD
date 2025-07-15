f3c5a9c8e70fa7673a1bb48d4271bc63076833d2
rksab <ramansarabha@gmail.com>
2025-07-15 18:00:15 +0530

    with dist

--- frontend/src/services/person.jsx
+++ frontend/src/services/person.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001/api/persons'
+const url = 'https://ci-cd-h555.onrender.com/api/persons'|| 'http://localhost:3001/api/persons'
 
 const getPersons = () => {
     const req = axios.get(url)
